8d7392ca9c77b02ba42aaa654db6ecab9ecc4605
Telmo Guibor <tlguibor@gmail.com>
2023-02-16 15:00:12 -0400

    criacao do projeto minimal api

---
+++ WebApiGit/Models/Info.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApiGit.Models
+{
+    public class Info
+    {
+        public int Id { get; set; }
+        public string DataHora { get; set; }
+
+        public Info()
+        {
+            Id = 0;
+            DataHora = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+        }
+    }
+}
---
+++ WebApiGit/Program.cs
@@ -0,0 +1,17 @@
+using WebApiGit.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.MapGet("/info", () =>
+{
+    Info info = new Info();
+    return info;
+});
+
+app.Run();
